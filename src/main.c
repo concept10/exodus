3a62e809e74381885d69bdbb6303a3ddb602d2bd
concept10 <tenthconcept@gmail.com>
2023-08-08 22:43:33 -0700

    not working

--- src/main.c
+++ src/main.c
@@ -21,8 +21,6 @@
 
 #include "config.h"
 
-#include <glib/gi18n.h>
-
 #include "exodus-application.h"
 
 int
@@ -31,10 +29,6 @@ main (int   argc,
 {
 	g_autoptr(ExodusApplication) app = NULL;
 	int ret;
-
-	bindtextdomain (GETTEXT_PACKAGE, LOCALEDIR);
-	bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
-	textdomain (GETTEXT_PACKAGE);
 
 	app = exodus_application_new ("io.outlaw.exodus", G_APPLICATION_DEFAULT_FLAGS);
 	ret = g_application_run (G_APPLICATION (app), argc, argv);
